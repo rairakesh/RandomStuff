5eee3f5b8c5a5e5eef54b4be45a5d05b4d23a720
Rakesh <rairakesh@gmail.com>
2017-08-01 08:31:54 -0400

    Moved test

--- src.test.java/com/test/src/siyonllc/randomexample/FizzBuzzTest.java
+++ src.test.java/com/test/src/siyonllc/randomexample/FizzBuzzTest.java
@@ -1,4 +1,4 @@
-package com.src.siyonllc.randomexample;
+package com.test.src.siyonllc.randomexample;
 
 import static org.junit.Assert.*;
 
@@ -7,6 +7,8 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.src.siyonllc.randomexample.FizzBuzz;
 
 public class FizzBuzzTest {
 
